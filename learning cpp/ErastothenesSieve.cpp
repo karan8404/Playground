96e7cb727a440115d50991b657adbe65825df3f0
karan8404 <114310500+karan8404@users.noreply.github.com>
2022-12-27 13:12:16 +0530

    completed learning c++

---
+++ learning cpp/ErastothenesSieve.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+#include <chrono>
+using namespace std::chrono;
+
+// creates the sieve of erastothenes
+void createSieve(vector<bool> &sieve)
+{
+    sieve[0] = false;
+    sieve[1] = false;
+    int limit = floor(sqrt(sieve.size()));
+
+    for (int i = 2; i <= limit; i++)
+    {
+        if (sieve[i] == false)
+            continue;
+        for (int j = i * i; j <= sieve.size(); j += i)
+        {
+            sieve[j] = false;
+        }
+    }
+}
+
+// prints the array in a well organized manner
+void printSieve(vector<bool> &sieve)
+{
+    for (int i = 1; i < sieve.size(); i++)
+    {
+        cout << i << ": " << sieve[i];
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    cout << "Enter the number till which primes are to be found\n";
+    int n = 100'000'000;
+    // cin >> n;
+    n = n + 1;
+    vector<bool> sieve;
+    sieve.reserve(n);
+    sieve.assign(n, true);
+
+    auto startTime = high_resolution_clock::now();
+
+    createSieve(sieve);
+    auto timeTaken = duration_cast<microseconds>(high_resolution_clock::now() - startTime);
+
+    // printSieve(sieve);
+    cout << "Time taken: " << (double)timeTaken.count() / 1000000 << " seconds\n";
+}
---
+++ learning cpp/primes.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+#include <chrono>
+using namespace std::chrono;
+
+// Checks if given number is prime.
+bool isPrime(int num)
+{
+    int z = floor(sqrt(num));
+    for (int i = 2; i <= z; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// marks non-prime numbers false
+void markPrimes(vector<bool> &primes)
+{
+    primes[0] = false;
+    primes[1] = false;
+    for (int i = 2; i < primes.size(); i++)
+    {
+        if (!isPrime(i))
+            primes[i] = false;
+    }
+}
+
+// prints the array in a well organized manner
+void printPrimes(vector<bool> &primes)
+{
+    for (int i = 1; i < primes.size(); i++)
+    {
+        cout << i << ": " << (primes[i] == true) ? "true" : "false";
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    cout << "Enter the number till which primes are to be found\n";
+    int n = 100'000'000;
+    // cin >> n;
+    n = n + 1;
+    vector<bool> primes;
+    primes.reserve(n);
+    primes.assign(n, true);
+
+    auto startTime = high_resolution_clock::now();
+
+    markPrimes(primes);
+    auto timeTaken = duration_cast<microseconds>(high_resolution_clock::now() - startTime);
+
+    // printPrimes(primes);
+    cout << "Time taken: " << (double)timeTaken.count() / 1000000 << " seconds\n";
+}
